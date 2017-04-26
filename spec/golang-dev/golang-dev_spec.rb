669ed551e2ff79850df4b3a40c5e590404346189
Stan Dorsett <stan.dorsett@gmail.com>
2017-04-25 22:14:37 -0500

    update spec/golang-dev/golang-dev_spec.rb to describe versions expected

--- spec/golang-dev/golang-dev_spec.rb
+++ spec/golang-dev/golang-dev_spec.rb
@@ -1,38 +1,46 @@
 require 'spec_helper'
 
-# check that zookeeper is installed and the service is started
+# check that zookeeper is installed
 describe package('zookeeper') do
-  it { should be_installed }
+  it { should be_installed.with_version('3.4.5') }
 end
 
+# check that zookeeper service is started and enabled to start on boot
 describe service('zookeeper-server') do
   it { should be_enabled }
   it { should be_running }
 end
 
-# check that postgresql 9.4 is installed and the service is started
+# check that zookeeper port is listening
+describe port(2181) do
+  it { should be_listening }
+end
+
+# check that postgresql 9.4 is installed
 describe package('postgresql94-server') do
   it { should be_installed }
 end
 
+# check that postgresql 9.4 service is started and enabled to start on boot
 describe service('postgresql-9.4') do
   it { should be_enabled }
   it { should be_running }
 end
 
-# check that rabbitmq is installed and the service is started
-describe package('rabbitmq-server') do
-  it { should be_installed }
+# check that postgresql port is listening
+describe port(5432) do
+  it { should be_listening }
 end
 
+# check that rabbitmq is installed
+describe package('rabbitmq-server') do
+  it { should be_installed.with_version('3.3.5') }
+end
+
+# check that rabbitmq service is started and enabled to start on boot
 describe service('rabbitmq-server') do
   it { should be_enabled }
   it { should be_running }
-end
-
-# check that 'go version' reports the expected version of golang
-describe command('/usr/local/go/bin/go version') do
-  its(:stdout) { should match /go version go1.8.1 linux\/amd64/ }
 end
 
 # check that rabbitmq server admin port is listening
@@ -40,7 +48,8 @@ describe port(15672) do
   it { should be_listening }
 end
 
-# check that postgresql port is listening
-describe port(5432) do
-  it { should be_listening }
+# check that 'go version' reports the expected version of golang
+describe command('/usr/local/go/bin/go version') do
+  its(:stdout) { should match /go version go1.8.1 linux\/amd64/ }
 end
+
